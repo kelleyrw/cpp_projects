5a72c696ec07d46933fbd92690f1e0e0f0c0ca4f
kelleyrw <kelleyrw@gmail.com>
2015-08-15 10:52:40 -0700

    sync with randy

--- libs/language_tools/test/source/test_language_tools.cpp
+++ libs/language_tools/test/source/test_language_tools.cpp
@@ -10,11 +10,19 @@ bool const test_string_contains()
 	return result;
 }
 
+bool const test_string_remove_all()
+{
+	bool result = true;
+	result &= lt::string_remove_all("Ryan Ward Kelley, Randall Scott Kelley", "Kelley")=="Ryan Ward , Randall Scott ";
+	return result;
+}
+
 int main()
 {
     std::cout << "testing lt::language_tools..." << std::endl;
 
 	bool result = true;
 	result &= test_string_contains();
+	result &= test_string_remove_all();
 	return result ? EXIT_SUCCESS : EXIT_FAILURE;
 }
